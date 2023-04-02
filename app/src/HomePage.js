2d016f9e9dfdd05247db069834de8d0fa4c23492
Levin <levinkaus@gmail.com>
2023-04-02 15:11:54 +0700

    Updated HomePage to show test API reslut

--- app/src/HomePage.js
+++ app/src/HomePage.js
@@ -1,25 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
   const navigateTo = useNavigate();
-  const [data, setData] = useState('');
+  const [data, setData] = useState("");
 
   useEffect(() => {
     (async function () {
-      const { text } = await( await fetch(`/api/signUp`)).json();
+      const { text } = await (await fetch(`/api/signUp`)).json();
       setData(text);
     })();
-  });
+  }, []);
 
   return (
-    <p 
-      onClick={() => {
-        navigateTo("/signup");
-      }}
-    >
-      Click to go to sign up
-    </p>,
-    <p>{data}</p>
+    <div>
+      <p
+        onClick={() => {
+          navigateTo("/signup");
+        }}
+      >
+        Click to go to sign up
+      </p>
+      <p>{data}</p>
+    </div>
   );
 }
