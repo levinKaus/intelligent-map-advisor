45434a68e11697a787939e5ec2c8b3e0c0424f8c
Levin <levinkaus@gmail.com>
2023-04-03 14:35:11 +0700

    Simplified app for testing

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,39 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./HomePage";
-import SignupPage from "./SignupPage";
-import SignupTest from "./SignupTest";
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({username: '', email: '', password: ''});
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch('/api/signUp', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    });
+    const data = await response.json();
+    console.log(data); // do something with the response data
+  }
+
+
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/signup" element={<SignupPage />}></Route>
-          <Route path="/signupTest" element={<SignupTest />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <form onSubmit={handleSubmit}>
+        <di>
+          <h4>Username</h4>
+          <input type="text" name="username" value={formData.username} onChange={(e) => setFormData({...formData, username: e.target.value})} />
+        </di>
+        <di>
+        <h4>Email</h4>
+          <input type="email" name="email" value={formData.email} onChange={(e) => setFormData({...formData, email: e.target.value})} />
+        </di>
+        <di>
+        <h4>Password</h4>
+          <input type="text" name="password" value={formData.password} onChange={(e) => setFormData({...formData, password: e.target.value})} />
+        </di>
+        <button type="submit">Submit</button>
+    </form>
   );
 }
 
