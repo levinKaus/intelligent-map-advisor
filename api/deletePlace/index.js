c8c9e721a034153af32f06e5b731d860784347dd
Levin <levinkaus@gmail.com>
2023-04-04 20:58:41 +0700

    Added deletePlace API

---
+++ api/deletePlace/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const AZURE_COSMOSDB_CONNECTION_STRING = process.env['AZURE_COSMOSDB_CONNECTION_STRING'];
+
+module.exports = async function (context, req) {
+  const client = new MongoClient(AZURE_COSMOSDB_CONNECTION_STRING);
+  await client.connect();
+  if (!AZURE_COSMOSDB_CONNECTION_STRING) {throw Error("Azure Cosmos DB Connection string not found.");}
+  if (req.body.userID !== "" && req.body.name !== "" && req.body.longitude !== "" && req.body.latitude !== "") {
+    // Get reference of database and collection
+    let db = await client.db('mapadvisor-db');
+    let collection = await db.collection('places');
+
+    // Check if user has already saved places
+    const foundPlaces = await collection.findOne({userID: req.body.userID });
+
+    if(foundPlaces == null) {
+      context.res.status(400).send({ message: "There are no saved places yet." });
+    } else {
+      // Remove place from placesList array
+      const index = foundPlaces.placesList.indexOf({
+        name: req.body.name,
+        longitude: req.body.longitude,
+        langitude: req.body.langitude
+      })
+      if (index > -1) { foundPlaces.placesList.splice(index, 1); }
+      await collection.updateOne(
+        { userID: req.body.userID },
+        { $set: { 'placesList': foundPlaces.placesList } }
+      )
+      context.res.status(200).send({ message: "The place was deleted" });
+    }
+  } else {
+    context.res.status(400).send({ message: "UserId and/or name and/or longitude and/or langitude is missing." });
+  }
+}
--- app/src/TestAPIs.js
+++ app/src/TestAPIs.js
@@ -3,16 +3,30 @@ import { useNavigate } from "react-router-dom";
 
 export default function TestAPIs() {
   const navigateTo = useNavigate()
-  const [formData, setFormData] = useState({userID: '', name: '', longitude: '', langitude: ''});
+  const [formDataSavePlace, setFormDataSavePlace] = useState({userID: '', name: '', longitude: '', langitude: ''});
+  const [formDataDeletePlace, setFormDeletePlace] = useState({userID: '', name: '', longitude: '', langitude: ''});
 
-  const handleSubmit = async (event) => {
+  const handleSubmitSavePlace = async (event) => {
     event.preventDefault();
     const response = await fetch('/api/savePlace', {
         method: 'POST',
         headers: {
         'Content-Type': 'application/json'
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formDataSavePlace)
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
+  const handleSubmitDeletePlace = async (event) => {
+    event.preventDefault();
+    const response = await fetch('/api/deletePlace', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formDataDeletePlace)
     });
     const data = await response.json();
     console.log(data);
@@ -28,25 +42,49 @@ export default function TestAPIs() {
       >
         Click to go to home
       </p>
+      
+
       <h2>Test savePlace</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmitSavePlace}>
       <div>
         <h4>userID</h4>
-        <input type="text" name="userID" value={formData.userID} onChange={(e) => setFormData({...formData, userID: e.target.value})} />
+        <input type="text" name="userID" value={formDataSavePlace.userID} onChange={(e) => setFormDataSavePlace({...formDataSavePlace, userID: e.target.value})} />
       </div>
       <div>
         <h4>Name</h4>
-        <input type="text" name="name" value={formData.name} onChange={(e) => setFormData({...formData, name: e.target.value})} />
+        <input type="text" name="name" value={formDataSavePlace.name} onChange={(e) => setFormDataSavePlace({...formDataSavePlace, name: e.target.value})} />
       </div>
       <div>
         <h4>Longitude</h4>
-        <input type="text" name="longitude" value={formData.longitude} onChange={(e) => setFormData({...formData, longitude: e.target.value})} />
+        <input type="text" name="longitude" value={formDataSavePlace.longitude} onChange={(e) => setFormDataSavePlace({...formDataSavePlace, longitude: e.target.value})} />
       </div>
       <div>
         <h4>Langitude</h4>
-        <input type="text" name="langitude" value={formData.langitude} onChange={(e) => setFormData({...formData, langitude: e.target.value})} />
+        <input type="text" name="langitude" value={formDataSavePlace.langitude} onChange={(e) => setFormDataSavePlace({...formDataSavePlace, langitude: e.target.value})} />
       </div>
       <button type="submit">Save Place</button>
+      </form>
+
+
+      <h2>Test deletePlace</h2>
+      <form onSubmit={handleSubmitDeletePlace}>
+      <div>
+        <h4>userID</h4>
+        <input type="text" name="userID" value={formDataDeletePlace.userID} onChange={(e) => setFormDeletePlace({...formDataDeletePlace, userID: e.target.value})} />
+      </div>
+      <div>
+        <h4>Name</h4>
+        <input type="text" name="name" value={formDataDeletePlace.name} onChange={(e) => setFormDeletePlace({...formDataDeletePlace, name: e.target.value})} />
+      </div>
+      <div>
+        <h4>Longitude</h4>
+        <input type="text" name="longitude" value={formDataDeletePlace.longitude} onChange={(e) => setFormDeletePlace({...formDataDeletePlace, longitude: e.target.value})} />
+      </div>
+      <div>
+        <h4>Langitude</h4>
+        <input type="text" name="langitude" value={formDataDeletePlace.langitude} onChange={(e) => setFormDeletePlace({...formDataDeletePlace, langitude: e.target.value})} />
+      </div>
+      <button type="submit">Delete Place</button>
       </form>
     </div>
   );
