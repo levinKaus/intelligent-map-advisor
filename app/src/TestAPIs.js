78d201b8e73183a2a36cfda6f2273b57492debe8
ConanCoM (Leon) <79465272+conancom@users.noreply.github.com>
2023-04-20 09:23:09 +0700

    Nested set on array

--- app/src/TestAPIs.js
+++ app/src/TestAPIs.js
@@ -156,11 +156,29 @@ export default function TestAPIs() {
       <form onSubmit={handleSubmitGeneratePlaces}>
         <div>
           <h4>Search Area/Location Name</h4>
-          <input type="text" name="location" value={formDataGeneratePlaces.location} onChange={(e) => setFormDataGeneratePlaces({ ...formDataGeneratePlaces, places[0].location: e.target.value })} />
+          <input type="text" name="location" value={formDataGeneratePlaces.location} onChange={(e) => setFormDataGeneratePlaces({
+            ...formDataGeneratePlaces,
+            places: [
+              {
+                ...formDataGeneratePlaces.places[0],
+                location: e.target.value
+              },
+              ...formDataGeneratePlaces.places.slice(1)
+            ]
+          })} />
         </div>
         <div>
           <h4>What to do:</h4>
-          <input type="text" name="action" value={formDataGeneratePlaces.action} onChange={(e) => setFormDataGeneratePlaces({ ...formDataGeneratePlaces, places[0].action: e.target.value })} />
+          <input type="text" name="action" value={formDataGeneratePlaces.action} onChange={(e) => setFormDataGeneratePlaces({
+            ...formDataGeneratePlaces,
+            places: [
+              {
+                ...formDataGeneratePlaces.places[0],
+                action: e.target.value
+              },
+              ...formDataGeneratePlaces.places.slice(1)
+            ]
+          })} />
         </div>
         <button type="submit">Generate Places</button>
       </form>
