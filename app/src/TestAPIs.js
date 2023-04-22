cb1f838aeabacebf65046c1b2d139070aa1a69b6
conancom <cnoanplays@gmail.com>
2023-04-22 18:40:28 +0700

    fix wrong value

--- app/src/TestAPIs.js
+++ app/src/TestAPIs.js
@@ -165,11 +165,11 @@ export default function TestAPIs() {
       <form onSubmit={handleSubmitGeneratePlaces}>
         <div>
           <h4>Search Area/Location Name</h4>
-          <input type="text" name="location" value={formDataGeneratePlaces.location} onChange={(e) => setformLocationDataGeneratePlaces(e.target.value)} />
+          <input type="text" name="location" value={formLocationDataGeneratePlaces} onChange={(e) => setformLocationDataGeneratePlaces(e.target.value)} />
         </div>
         <div>
           <h4>What to do:</h4>
-          <input type="text" name="action" value={formDataGeneratePlaces.action} onChange={(e) => setformActionDataGeneratePlaces(e.target.value)} />
+          <input type="text" name="action" value={formActionDataGeneratePlaces} onChange={(e) => setformActionDataGeneratePlaces(e.target.value)} />
         </div>
         <button type="submit">Generate Places</button>
       </form>
